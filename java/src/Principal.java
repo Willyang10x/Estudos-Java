6fee1f4e69987dddd82fa7dff2bb1454dd732d91
Willyang10x <willyangabrielfxt@gmail.com>
2026-04-27 23:06:08 -0300

    Codigo simples com ação de acelerar com soma 10

--- java/src/Principal.java
+++ java/src/Principal.java
@@ -1,28 +1,52 @@
-class ContaBancaria {
-    String titular;
-    double saldo;
+// class ContaBancaria {
+//     String titular;
+//     double saldo;
 
-    void depositar(double valor) {
-        saldo = saldo + valor;
-    }
+//     void depositar(double valor) {
+//         saldo = saldo + valor;
+//     }
 
-    void sacar(double valor) {
-        saldo = saldo - valor;
+//     void sacar(double valor) {
+//         saldo = saldo - valor;
+//     }
+// }
+
+// public class Principal {
+//     public static void main(String[] args) {
+//         ContaBancaria conta = new ContaBancaria();
+        
+//         conta.titular = "João";
+//         conta.saldo = 100.0;
+
+//         conta.depositar(50.0);
+        
+//         conta.sacar(30.0);
+
+//         System.out.println(conta.titular);
+//         System.out.println(conta.saldo);
+//     }
+// }
+
+class Carro {
+    String modelo;
+    int velocidade;
+
+    void acelerar() {
+        velocidade = velocidade + 10;
     }
 }
 
 public class Principal {
     public static void main(String[] args) {
-        ContaBancaria conta = new ContaBancaria();
+        Carro meuCarro = new Carro();
         
-        conta.titular = "João";
-        conta.saldo = 100.0;
+        meuCarro.modelo = "Fusca";
+        meuCarro.velocidade = 0;
 
-        conta.depositar(50.0);
-        
-        conta.sacar(30.0);
+        meuCarro.acelerar();
+        meuCarro.acelerar();
 
-        System.out.println(conta.titular);
-        System.out.println(conta.saldo);
+        System.out.println(meuCarro.modelo);
+        System.out.println(meuCarro.velocidade);
     }
 }
